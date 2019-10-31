f78dc3c6dc53af8ad9774a68912a7abd5334c222
Guilherme-Bodart <guilhermebodart73@gmail.com>
2019-10-31 18:30:41 -0300

    uva11340 - time limit
    Tentar fazer o codigo mais rapido, ou mudar de linguagem

---
+++ teste.py
@@ -0,0 +1,7 @@
+listaQuant =[]
+frase ="vai se fuder"
+listaLetra = ['a','b','c']
+for r in range(len(listaLetra)):
+    listaQuant.append(frase.count(listaLetra[r]))
+
+print(listaQuant)
--- uva11340.py
+++ uva11340.py
@@ -1,1 +1,34 @@
-Nao esta vazio
+testeNum = int(input(""))
+i = 0
+
+while (i < testeNum):
+
+    listaLetra = []
+    listaValor = []
+    listaQuant = []
+    j=0
+    k=0
+    valorTotal = 0
+    frase = ""
+
+    quantidadeValores = int(input(""))    
+    while(j < quantidadeValores):
+        valores = input("").split()
+        listaLetra.append(valores[0])
+        listaValor.append(int(valores[1]))
+        j = j + 1
+
+    quantidadeLinhas = int(input(""))    
+    while(k < quantidadeLinhas):
+        linha = input("")
+        frase = frase + " " + linha
+        k = k + 1
+    
+    for r in range(0,len(listaLetra)):
+        quantidadeLetra = frase.count(listaLetra[r])
+        valorTotal = valorTotal + (listaValor[r] * quantidadeLetra)
+
+    i = i + 1
+    print("%.2f$"%(valorTotal/100))
+    
+
