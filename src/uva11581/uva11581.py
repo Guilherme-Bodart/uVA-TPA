39c322dee206aa4414d6a861711215b6b6b973b2
Guilherme-Bodart <guilhermebodart73@gmail.com>
2019-11-06 19:13:26 -0300

    uva11581 - Accept + Comments

--- src/uva11581/uva11581.py
+++ src/uva11581/uva11581.py
@@ -1,3 +1,11 @@
+# uva10107.py
+# UVa 10107 - What is the Median?
+
+# @authors Douglas Bolis, Guilherme Bodart e Jadson Pereira.
+
+# Get-Content entrada.txt | python uvaXXXXX.py > saida.txt  >>>>Rodando o programa com entrada por .txt
+
+#Verifica se a matriz esta preenchida só com zero
 def tudoZero(matriz):
     for i in range(3):
         for j in range(3):
@@ -5,9 +13,10 @@ def tudoZero(matriz):
                 return False
     return True
 
-
+#Calcula o grid de cada cedula da matriz usando seus vizinhos (soma dos vizinhos)%2
 def grid(matriz):
     matrizG = [[0,0,0],[0,0,0],[0,0,0]]
+    #Cada caso possivel com seus vizinhos
     matrizG[0][0] = (matriz[0][1]+matriz[1][0])%2
     matrizG[0][1] = (matriz[0][0]+matriz[1][1]+matriz[0][2])%2
     matrizG[0][2] = (matriz[0][1]+matriz[1][2])%2
@@ -21,22 +30,20 @@ def grid(matriz):
     return matrizG
 
 def main():
+    #inicia matriz
     matriz = [[0,0,0],[0,0,0],[0,0,0]]
     num = int(input())
-    branco = input()
     for n in range(num):
         for i in range(3):
-            try:
-                string = input()
-            except EOFError:
-                break
-            if(string!=''):
-                print(string)
-                for j in range(3):
-                    matriz[i][j] = int(string[j])
+            string = input()
+            if(string==''): #Pula linha lida vazia
+                string = input() #Pega uma string de tamanho 3
+            for j in range(3):
+                matriz[i][j] = int(string[j]) #Valores da matriz sendo preenchidos
                 
             
         contador = -1
+        #While que verifica quantas vezes a funcao grid foi usada até todos os valores ficarem 0
         while(not tudoZero(matriz)):
             matriz = grid(matriz)
             contador = contador + 1
