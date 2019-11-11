93024a04eb7f6be5e8d13d2d06cd9d5eabddc929
Guilherme-Bodart <guilhermebodart73@gmail.com>
2019-11-11 16:25:08 -0200

    uva00939 Completed - Accepted

--- src/uva00939/uva00939.py
+++ src/uva00939/uva00939.py
@@ -1,7 +1,6 @@
 def arvoreGenetica(mapa):
     tam = 0
     for j in mapa:
-
         if (mapa[j][0]==""):
             verifGenetica(j,mapa)
     return mapa
@@ -10,24 +9,29 @@ def arvoreGenetica(mapa):
 def verifGenetica(pessoa,mapa):
     primeiro = "";
     segundo = "";
+    if mapa[pessoa][1] in mapa:
+        primeiro = mapa[pessoa][1]
     if mapa[pessoa][2] in mapa:
-        primeiro = mapa[pessoa][2]
-    if mapa[pessoa][3] in mapa:
-        segundo = mapa[pessoa][3]
+        segundo = mapa[pessoa][2]
     if(mapa[primeiro][0] == ""):
-        verifGenetica(primeiro)
+        verifGenetica(primeiro,mapa)
     if(mapa[segundo][0] == ""):
-        verifGenetica(segundo) 
-    if (mapa[segundo][0] == "non-existent"):
-        mapa[pessoa][0] = "non-existent"
-    elif (mapa[primeiro][0] == "non-existent"):
-        mapa[pessoa][0] = "non-existent"
+        verifGenetica(segundo,mapa) 
+    if (mapa[primeiro][0] == "dominant" and mapa[segundo][0] == "non-existent"):
+        mapa[pessoa][0] = "recessive"
+    elif (mapa[primeiro][0] == "non-existent" and mapa[segundo][0] == "dominant"):
+        mapa[pessoa][0] = "recessive" 
     elif (mapa[primeiro][0] == "dominant" and mapa[segundo][0] == "recessive"):
         mapa[pessoa][0] = "dominant" 
     elif (mapa[primeiro][0] == "recessive" and mapa[segundo][0] == "dominant"):
         mapa[pessoa][0] = "dominant"
+    elif (mapa[primeiro][0] == "dominant" and mapa[segundo][0] == "dominant"):
+        mapa[pessoa][0] = "dominant"
     elif (mapa[primeiro][0] == "recessive" and mapa[segundo][0] == "recessive"):
         mapa[pessoa][0] = "recessive"
+    else:
+        mapa[pessoa][0] = "non-existent"
+    
     return mapa
 
 def main():
@@ -36,8 +40,8 @@ def main():
     num = int(input())
     listaOrd = []
     for i in range(num):
-        lista = ["","","","",""]
-        lista2 = ["","","","",""]
+        lista = ["","",""]
+        lista2 = ["","",""]
         string = input().split()
         primeiro = string[0];
         segundo = string[1];
@@ -47,14 +51,14 @@ def main():
         if (segundo=="dominant" or segundo=="recessive" or segundo=="non-existent") :
             mapa[primeiro][0] = segundo
         else:
-            mapa[primeiro][1] = segundo
+            mapa[primeiro].append(segundo)
             if (not segundo in mapa):
                 mapa[segundo] = lista2;
                 listaOrd.append(segundo)
-            if (mapa[segundo][2]==""):
-                mapa[segundo][2]=primeiro
+            if (mapa[segundo][1]==""):
+                mapa[segundo][1]=primeiro
             else:
-                mapa[segundo][3]=primeiro
+                mapa[segundo][2]=primeiro
     
     mapa = arvoreGenetica(mapa)
     
