c277dab13ffa38d9be9283f2a9c60820ac6538e9
Guilherme-Bodart <guilhermebodart73@gmail.com>
2019-11-17 10:30:40 -0300

    10264 tle

--- 10264.py
+++ 10264.py
@@ -7,31 +7,37 @@ while(True):
     cuboSoma = {}
     listaVizinhos = []
     for i in range((num*num)-1):
-        peso = int(input())
-        numB = bin(i)[2:]
+        try:
+            peso = int(input())
+        except EOFError:
+            break
+        numB = bin(i)[2:]        
         chave = str(0)*(num-len(numB)) + numB
-        cubo[chave] = peso;
+        cubo[chave] = peso
 
     for key in cubo:    
         soma = 0
         for k in range(num):
             if(key[k])=='0':
                 string = key[0:k]+ '1' + key[k+1:]
-                soma = soma + cubo[string]
+                if string in cubo:
+                    soma = soma + cubo[string]
+                    listaVizinhos.append(string)
             else:
                 string = key[0:k]+ '0' + key[k+1:]
-                soma = soma + cubo[string]                
-            listaVizinhos.append(string)
+                if string in cubo:
+                    soma = soma + cubo[string]
+                    listaVizinhos.append(string)
 
         cuboSoma[key] = soma
-    print(listaVizinhos)
-    for s in cuboSoma:
-        print(s,cuboSoma[s])
     maior = 0
     y = 0
     for s in cuboSoma:        
         for e in range(num):
-            soma = cuboSoma[s] + cuboSoma[str(listaVizinhos[(y*num + e)])]
+            try:
+                soma = cuboSoma[s] + cuboSoma[str(listaVizinhos[(y*num + e)])]
+            except:
+                soma = cuboSoma[s]            
             if(soma>maior):
                 maior = soma      
         y = y + 1 
